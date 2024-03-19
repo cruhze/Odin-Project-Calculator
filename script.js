53bec9a4a3a55bc246f0b04540aba6ff7b70bb93
cruhze <cruhze@gmail.com>
2024-03-18 20:59:43 -0400

    Fully added light and night mode switch with svg icons

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function add(a,b){
-    return a + b
+const switcher = document.querySelector(".switch"); // Switches between Dark and Light mode
+switcher.addEventListener("click", function () {
+  const nightIcon = document.querySelector(".night");
+  const lightIcon = document.querySelector(".day");
+  document.body.classList.toggle("dark-mode"); // Toggles Dark mode to body
+  nightIcon.classList.toggle("hidden");
+  lightIcon.classList.toggle("hidden");
+});
+
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b){
-    return a - b
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b){
-    if(b===0){
-        return 'Cannot be divided by zero'
-    }else{
-        return a / b
-    }
+function divide(a, b) {
+  if (b === 0) {
+    return "Cannot be divided by zero";
+  } else {
+    return a / b;
+  }
 }
-
-console.log(add(1,4))
